33f48fbac196ec8e072ed620dc4b73482bbd1289
jakebottrall <jakebottrall@gmail.com>
2023-04-25 22:33:24 -0700

    remove jest.setup

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jest-environment-jsdom",
   verbose: true,
 };
